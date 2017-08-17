97cd8fa20c575e2ffb9cfc9e6c35f9e36c6b1b37
DM1JZ <mail@dm1jz.de>
2017-08-17 21:47:00 +0200

    Fix Timing, 8MHz Crystal

--- Software/main.c
+++ Software/main.c
@@ -2,12 +2,16 @@
 #include <util/delay.h>
 #include "dds.h"
 
+#define TX_Frequency 77500
+
 void sendTimeCode(void);
 
 //Datenstrom
 //					MESZ			Begin Zeit		Min Einer		Min Einer	Min Zehner		Parität			Std Einer	Std Einer		Std Zehner		Parität	  	KaltagEiner 	KaltagEiner 	KaltagZehner Wotag			Wotag			MonEiner		MonEiner		MonEiner	JahrEiner		JahrEiner		JahrEiner		JahrZehner	Parität
-uint64_t timeCode = (1ULL << 17) | (1ULL << 20) | (1ULL << 21) | (1ULL << 23) | (1ULL << 25) | (1ULL << 28) | (1ULL << 29) | (1ULL << 31) | (1ULL << 33) | (1ULL << 35)| (1ULL << 39) | (1ULL << 36) | (1ULL << 41) | (1ULL << 43) | (1ULL << 44) | (1ULL << 47) | (1ULL << 46) | (1ULL << 45) | (1ULL << 52) | (1ULL << 51) | (1ULL << 50) | (1ULL << 54) | (0ULL << 58);
+//uint64_t timeCode = (1ULL << 17) | (1ULL << 20) | (1ULL << 21) | (1ULL << 23) | (1ULL << 25) | (1ULL << 28) | (1ULL << 29) | (1ULL << 31) | (1ULL << 33) | (1ULL << 35)| (1ULL << 39) | (1ULL << 36) | (1ULL << 41) | (1ULL << 43) | (1ULL << 44) | (1ULL << 47) | (1ULL << 46) | (1ULL << 45) | (1ULL << 52) | (1ULL << 51) | (1ULL << 50) | (1ULL << 54) | (0ULL << 58);
 //15:15Uhr 29.7.17
+//uint8_t timeCode = 0xF0;
+uint64_t timeCode = 0b01010000000101100100110001000010001011100100111100111010000;
 
 int main(void)
 {
@@ -21,11 +25,17 @@ int main(void)
 		
 	//DDS-Modul auf 77,5kHz einstellen
 	//...
-	DDS_setFrequency(80000);
+	DDS_setFrequency(TX_Frequency);
 	
 	while(1)
 	{
 		sendTimeCode();
+		/*
+		DDS_setFrequency(10);
+		_delay_ms(1000);
+		DDS_setFrequency(TX_Frequency);
+		_delay_ms(1000);
+		*/
 	}
 	
 	return 0;
@@ -34,28 +44,28 @@ int main(void)
 void sendTimeCode(void)
 {
 	//Zeichenstrom Bit für Bit abtasten
-	
-	for(uint8_t bitPosition = 59; bitPosition >= 0; bitPosition--)
+	for(int8_t bitPosition = 58; bitPosition >= 0; bitPosition--)
 	{
-		if((timeCode & (1 << bitPosition)) == 0)
+		if((timeCode & (1ULL << bitPosition)) == 0)
 		{
 			//Bit ist eine Null
 			//Träger für 100ms abschwächen
 			//Abschwächung durch Schalten eines Pins aktivieren für die entsprechende Zeitdauer
-			PORTB = 0;
+			DDS_setFrequency(10);
 			_delay_ms(100);
-			PORTB = 1;
+			DDS_setFrequency(TX_Frequency);
 			_delay_ms(900);
 		}
 		else
 		{
 			//Bit ist eine Eins
 			//Träger für 200ms abschwächen
-			PORTB = 0;
+			DDS_setFrequency(10);
 			_delay_ms(200);
-			PORTB = 1;
+			DDS_setFrequency(TX_Frequency);
 			_delay_ms(800);
 		}
-		
 	} 
+	//59. Bit: Dauerton
+	_delay_ms(1000);
 }
